bfbf03a2cc02df25c7d582bdf29e67bda19a3fdd
gileaddias <gfarias.gpf@gmail.com>
2025-06-24 17:36:21 -0300

    criando templates e configurando Tailwind CSS

--- appcrm/urls.py
+++ appcrm/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib.auth import views
+
+from core.views import index, sobre
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('sobre/', sobre, name='sobre'),
 ]
